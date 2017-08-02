1ee32c4ec057880c84abe88aa0bd1df546b5cb4f
Bowen <bowen.sui@hotmail.com>
2017-08-02 10:19:12 +0200

    create 1 basics.py

--- p/p/numpy/1 basics.py
+++ p/p/numpy/1 basics.py
@@ -1,5 +1,5 @@
 # NumPy's main object is the homogeneous multidimensional array. It is a table of elements (usually numbers), all of the same type, indexed by a tuple of positive integers. In NumPy dimensions are called axes. The number of axes is rank
 
 import numpy as np 
-a = np.arnage(15).reshape(3, 5)
+a = np.arange(15).reshape(3, 5)
 print(a)
