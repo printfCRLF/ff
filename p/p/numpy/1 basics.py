76fd1454397b14d7236bda900dfe0ad087f77fd5
Bowen <bowen.sui@hotmail.com>
2017-08-02 10:25:15 +0200

    common methods of ndarray

--- p/p/numpy/1 basics.py
+++ p/p/numpy/1 basics.py
@@ -3,3 +3,19 @@
 import numpy as np 
 a = np.arange(15).reshape(3, 5)
 print(a)
+
+# the dimensions of the array
+print('shape: {0}'.format(a.shape))
+
+# number of axes
+print('ndim: {0}'.format(a.ndim))
+
+print('dtype.name: {0}'.format(a.dtype.name))
+
+# the size in bytes of each element of the array
+print('itemsize: {0}'.format(a.itemsize))
+
+# the total number of elements of the array. This is equal to the product of the elements of shape
+print('size: {0}'.format(a.size))
+
+print('type(a): {0}'.format(type(a)))
